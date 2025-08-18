b58f208701a8012b132722e80d4d733a5835ccbf
xiaoyaliu00 <xiaoyaliu00@gmail.com>
2025-08-19 03:44:17 +0800

    bypass alist restriction

--- pkg/driver/util.go
+++ pkg/driver/util.go
@@ -32,10 +32,10 @@ func Date() string {
 }
 
 func isCalledByAlistV3() bool {
-	pc, _, _, ok := runtime.Caller(3)
-	if !ok {
-		return false
-	}
+	//_, _, _, ok := runtime.Caller(3)
+	//if !ok {
+//		return false
+//	}
 
 	//funcName := runtime.FuncForPC(pc).Name()
 	//return strings.Contains(funcName, "alist")
